f570c35199d0888a369f52f75053feac1d4ced2e
Durant Barthelemy <barthelemy.durant@gmail.com>
2018-10-08 18:11:04 +0200

    TreeView
    Bindé les donnée en BDD sur la TreeView pour créer la hiérachie(à revoir)

--- StageAfpa-master/WorldlineMobileTeamOrganizationChart/ViewModel/OrganizationChartViewModel.cs
+++ StageAfpa-master/WorldlineMobileTeamOrganizationChart/ViewModel/OrganizationChartViewModel.cs
@@ -13,28 +13,28 @@ namespace WorldlineMobileTeamOrganizationChart.ViewModel
 {
     class OrganizationChartViewModel:BindableBaseViewModel
     {
-        //#region //propiétés
+        #region //propiétés
         private List<StaffMember> _ManagerList;
         private List<StaffMember> _ProjectManagerList;
         private List<StaffMember> _DeveloperList;
 
-        //private string _Surname;
-        //private string _Name;
-        //private string _Mail;
-        //private string _Tel;
+        private string _Surname;
+        private string _Name;
+        private string _Mail;
+        private string _Tel;
 
 
 
-        //public string Surname { get => _Surname; set { _Surname = value; RaisePropertyChanged(nameof(Surname)); } }
-        //public string Name { get => _Name; set { _Name = value; RaisePropertyChanged(nameof(Name)); } }
-        //public string Mail { get => _Mail; set { _Mail = value; RaisePropertyChanged(nameof(Mail)); } }
-        //public string Tel { get => _Tel; set { _Tel = value; RaisePropertyChanged(nameof(Tel)); } }
+        public string Surname { get => _Surname; set { _Surname = value; RaisePropertyChanged(nameof(Surname)); } }
+        public string Name { get => _Name; set { _Name = value; RaisePropertyChanged(nameof(Name)); } }
+        public string Mail { get => _Mail; set { _Mail = value; RaisePropertyChanged(nameof(Mail)); } }
+        public string Tel { get => _Tel; set { _Tel = value; RaisePropertyChanged(nameof(Tel)); } }
         public List<StaffMember> managerList { get => _ManagerList; set { _ManagerList = value; RaisePropertyChanged(nameof(managerList)); } }
         public List<StaffMember> projectManagerList { get => _ProjectManagerList; set { _ProjectManagerList = value; RaisePropertyChanged(nameof(projectManagerList)); } }
         public List<StaffMember> developerList { get => _DeveloperList; set { _DeveloperList = value; RaisePropertyChanged(nameof(developerList)); } }
 
         public ICommand CommandAddStaffMembers { get; private set; }
-//#endregion
+        #endregion
 
         public OrganizationChartViewModel()
         {
@@ -44,36 +44,36 @@ namespace WorldlineMobileTeamOrganizationChart.ViewModel
 
 
 
-            //#region//Filtrage des données
-            //using (var contextManager = new StaffMembersContext())
-            //{
-            //    var manager = from fontionManager in contextManager.StaffMember
-            //                  where fontionManager.Fonction == Fonction.Manager
-            //                  select fontionManager;
+            #region//Filtrage des données
+            using (var contextManager = new StaffMembersContext())
+            {
+                var manager = from fontionManager in contextManager.StaffMember
+                              where fontionManager.Fonction == Fonction.Manager
+                              select fontionManager;
 
-            //    managerList = manager.ToList();
+                managerList = manager.ToList();
 
-            //    contextManager.SaveChanges();
-            //}
+                contextManager.SaveChanges();
+            }
 
-            //using (var contextProjectManager = new StaffMembersContext())
-            //{
-            //    var projectManager = from fonctionProjectManager in contextProjectManager.StaffMember
-            //                         where fonctionProjectManager.Fonction == Fonction.ChefDeProjet
-            //                         select fonctionProjectManager;
-            //    projectManagerList = projectManager.ToList();
+            using (var contextProjectManager = new StaffMembersContext())
+            {
+                var projectManager = from fonctionProjectManager in contextProjectManager.StaffMember
+                                     where fonctionProjectManager.Fonction == Fonction.ChefDeProjet
+                                     select fonctionProjectManager;
+                projectManagerList = projectManager.ToList();
 
 
-            //}
+            }
 
-            //using (var contextDeveloper = new StaffMembersContext())
-            //{
-            //    var developer = from fonctionDeveloper in contextDeveloper.StaffMember
-            //                    where fonctionDeveloper.Fonction == Fonction.ChefDeProjet
-            //                    select fonctionDeveloper;
-            //    developerList = developer.ToList();
-            //}
-            //#endregion
+            using (var contextDeveloper = new StaffMembersContext())
+            {
+                var developer = from fonctionDeveloper in contextDeveloper.StaffMember
+                                where fonctionDeveloper.Fonction == Fonction.ChefDeProjet
+                                select fonctionDeveloper;
+                developerList = developer.ToList();
+            }
+            #endregion
 
             CommandAddStaffMembers = new RelayCommand(OpenWindowsAddStaffMembers);
 
